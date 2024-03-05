42c667c1d38db35f3e8282e42b59971c8a82aac5
Mahmoud Ali <mahmoud.webapps@gmail.com>
2024-03-05 17:09:20 +0200

    add postgres configurations

--- e_service/settings.py
+++ e_service/settings.py
@@ -118,10 +118,20 @@ WSGI_APPLICATION = 'e_service.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD": config('DB_PASSWD'),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+        "CONN_MAX_AGE": 300,
+        "CONN_HEALTH_CHECKS": True,
+    }    
 }
 
 
