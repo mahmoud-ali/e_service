184c1071b016aeeea6da764ee87bc5906c02c3ce
Mahmoud <mahmoud.webapps@gmail.com>
2024-03-18 00:18:54 +0200

    test comments

--- company_profile/admin.py
+++ company_profile/admin.py
@@ -180,15 +180,15 @@ class TblCompanyProductionFactoryAdmin(LoggingAdminMixin,admin.ModelAdmin):
     
 class TblCompanyProductionLicenseAdmin(ExportActionMixin,LoggingAdminMixin,admin.ModelAdmin):
     fieldsets = [
-        (None, {"fields": ["company"]}),
+        (None, {"fields": ["company","license_no"]}),
         (_("General information"), {"fields": ["date",("start_date","end_date")]}),
         (_("Location information"), {"fields": [("state","locality","location","sheet_no","cordinates")]}),
         (_("Contract information"), {"fields": ["area","reserve","royalty","zakat","annual_rent","gov_rep","rep_percent","com_percent","contract_status","contract_file"]}),
      ]        
     exclude = ["created_at","created_by","updated_at","updated_by"]
     
-    list_display = ["company","sheet_no","date", "start_date", "end_date"]        
-    list_filter = ["date","sheet_no"]
+    list_display = ["company","license_no","sheet_no","date", "start_date", "end_date"]        
+    list_filter = ["date","license_no","sheet_no"]
     view_on_site = False
 
     class Media:
