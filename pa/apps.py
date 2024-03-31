c12193f4e471c4927bbc695664f27449f4e19f71
Mahmoud <mahmoud.webapps@gmail.com>
2024-04-01 01:15:38 +0200

    add model post save signals

--- pa/apps.py
+++ pa/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_save
 
 class PaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pa'
+
+    def ready(self) -> None:
+        #register & call signals
+        from .models import TblCompanyCommitment,TblCompanyRequest,TblCompanyPayment
+        from . import signals
+
+        post_save.connect(signals.commitement_generate_request,sender=TblCompanyCommitment, dispatch_uid="commitement_generate_request_signal_id")
+        post_save.connect(signals.send_email_after_add_request,sender=TblCompanyRequest, dispatch_uid="request_send_email_after_add_request_signal_id")
+        post_save.connect(signals.update_request_payment_state,sender=TblCompanyPayment, dispatch_uid="payment_update_request_payment_state_signal_id")
+
+        return super().ready()
+    
---
+++ pa/signals.py
@@ -0,0 +1,11 @@
+def commitement_generate_request(sender, **kwargs):
+    if not kwargs['raw']:
+        kwargs['instance'].generate_request() #instance = commitement instance
+
+def update_request_payment_state(sender, **kwargs):
+    if not kwargs['raw']:
+        kwargs['instance'].request.update_payment_state() #instance = payment instance
+
+def send_email_after_add_request(sender, **kwargs):
+    if not kwargs['raw']:
+        kwargs['instance'].send_email() #instance = request instance
