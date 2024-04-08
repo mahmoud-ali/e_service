590a1704240018dc8f8d38e22bfd9e25d2f58d15
Mahmoud <mahmoud.webapps@gmail.com>
2024-04-08 03:15:58 +0200

    add UserPermissionMixin & fix delete view

--- pa/views/application.py
+++ pa/views/application.py
@@ -10,7 +10,7 @@ from django.utils.translation import gettext_lazy as _
 
 from django.forms import inlineformset_factory
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 from django.contrib import messages
 
@@ -30,12 +30,22 @@ class TranslationMixin:
             response.set_cookie(settings.LANGUAGE_COOKIE_NAME,request.user.lang,max_age=datetime.timedelta(days=365))
 
         return response
+    
+class UserPermissionMixin(UserPassesTestMixin):
+    user_groups = []
 
-class ApplicationListView(LoginRequiredMixin,TranslationMixin,SingleTableView):
+    def test_func(self):
+        return self.request.user.groups.filter(name__in=self.user_groups).exists()
+    
+    def test_group(self,group):
+        return self.request.user.groups.filter(name=group).exists()
+
+class ApplicationListView(LoginRequiredMixin,UserPermissionMixin,TranslationMixin,SingleTableView):
     model = None
     table_class = None
     filterset_class = None
     title = None
+    user_groups = ['pa_data_entry','pa_manager']
     menu_name = ""
     relation_fields = []
     context_object_name = "apps"    
@@ -55,12 +65,13 @@ class ApplicationListView(LoginRequiredMixin,TranslationMixin,SingleTableView):
         if self.filterset_class:
             query = self.filterset_class(self.request.GET,queryset=query).qs
         return query.prefetch_related(*self.table_class.relation_fields)
-            
-class ApplicationCreateView(LoginRequiredMixin,CreateView):
+                
+class ApplicationCreateView(LoginRequiredMixin,UserPermissionMixin,CreateView):
     model = None
     form_class = None
     success_url = None
     title = None    
+    user_groups = ['pa_data_entry','pa_manager']
     menu_name = ""  
     template_name = "pa/application_add.html"    
     
@@ -77,7 +88,7 @@ class ApplicationCreateView(LoginRequiredMixin,CreateView):
         
         self.object.created_by = self.object.updated_by = self.request.user
 
-        if self.request.POST.get('_save_confirm'):
+        if self.request.POST.get('_save_confirm') and self.test_group('pa_manager'):
             self.object.state = STATE_TYPE_CONFIRM
 
         self.object.save()
@@ -85,11 +96,12 @@ class ApplicationCreateView(LoginRequiredMixin,CreateView):
         messages.add_message(self.request,messages.SUCCESS,_("Record saved successfully."))
         
         return HttpResponseRedirect(self.get_success_url())
-
-class ApplicationUpdateView(LoginRequiredMixin,UpdateView):
+                
+class ApplicationUpdateView(LoginRequiredMixin,UserPermissionMixin,UpdateView):
     model = None
     form_class = None
     title = None    
+    user_groups = ['pa_data_entry','pa_manager']
     menu_name = ""  
     menu_show_name = ""  
     template_name = "pa/application_add.html"    
@@ -122,7 +134,7 @@ class ApplicationUpdateView(LoginRequiredMixin,UpdateView):
         
         self.object.updated_by = self.request.user
 
-        if self.request.POST.get('_save_confirm'):
+        if self.request.POST.get('_save_confirm') and self.test_group('pa_manager'):
             self.object.state = STATE_TYPE_CONFIRM
 
         self.object.save()
@@ -131,10 +143,11 @@ class ApplicationUpdateView(LoginRequiredMixin,UpdateView):
                 
         return HttpResponseRedirect(self.get_success_url())
 
-class ApplicationReadonlyView(LoginRequiredMixin,SingleObjectMixin,View):
+class ApplicationReadonlyView(LoginRequiredMixin,UserPermissionMixin,SingleObjectMixin,View):
     model = None
     form_class = None
     title = None    
+    user_groups = ['pa_data_entry','pa_manager']
     menu_name = ""  
     menu_edit_name = ""
     menu_delete_name = ""
@@ -155,27 +168,40 @@ class ApplicationReadonlyView(LoginRequiredMixin,SingleObjectMixin,View):
         self.extra_context["object"] = obj
         return render(request, self.template_name, self.extra_context)
 
-class ApplicationDeleteView(DeleteView):
+class ApplicationDeleteView(LoginRequiredMixin,UserPermissionMixin,UpdateView):
     model = None
     form_class = None
     title = None    
+    user_groups = ['pa_manager']
     menu_name = ""  
     template_name = "pa/application_delete.html"    
 
     def dispatch(self, *args, **kwargs):
-        self.success_url = reverse_lazy(self.menu_name)
         self.extra_context = {
                             "menu_name":self.menu_name,
                             "title":self.title, 
          }
         return super().dispatch(*args, **kwargs)        
-
+                        
     def get_queryset(self):
         query = super().get_queryset()        
         return query.filter(state=STATE_TYPE_DRAFT)
-    
+
     def get(self,request,pk=0):     
         obj = self.get_object()
         self.extra_context["form"] = self.form_class(instance=obj)
         self.extra_context["object"] = obj
         return render(request, self.template_name, self.extra_context)
+    
+    def post(self,*args, **kwargs):     
+        obj = self.get_object()
+        self.success_url = reverse_lazy(self.menu_name,args=(obj.id,))    
+        return super().post(*args, **kwargs)        
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.delete()
+
+        messages.add_message(self.request,messages.SUCCESS,_("Record removed successfully."))
+                
+        return HttpResponseRedirect(self.get_success_url())
