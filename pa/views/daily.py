d3efa00ceceaa2f9f91b34310b37d3d311326dde
Mahmoud <mahmoud.webapps@gmail.com>
2024-04-29 20:36:49 +0200

    show balamce before report time

--- pa/views/daily.py
+++ pa/views/daily.py
@@ -45,64 +45,76 @@ class PaDailyView(LoginRequiredMixin,TranslationMixin,View):
             currency = form.cleaned_data["currency"]
             from_dt = form.cleaned_data["from_dt"]
             to_dt = form.cleaned_data["to_dt"]
-            request_qs = TblCompanyRequestDetail.objects.filter(
-                    request_master__created_at__date__gte=from_dt,
-                    request_master__created_at__date__lte=to_dt,
+            request_qs_all = TblCompanyRequestDetail.objects.filter(
                     request_master__state=STATE_TYPE_CONFIRM,
                 ).annotate(
                     exchange_rate=Value('1.0', output_field=FloatField()),
                     type=Value('request', output_field=CharField()),
                 )
             if company:
-                request_qs = request_qs.filter(
+                request_qs_all = request_qs_all.filter(
                         request_master__commitment__company=company,
                 )
             if currency:
-                request_qs = request_qs.filter(
+                request_qs_all = request_qs_all.filter(
                         request_master__currency=currency,
                 )
-            request_qs
+            request_qs = request_qs_all.filter(
+                    request_master__created_at__date__range=(from_dt,to_dt)
+            )
+
             request_total = request_qs.aggregate(total=Sum("amount"))['total']
+            request_total_befor = request_qs_all \
+                .filter(request_master__created_at__date__lt=from_dt) \
+                .aggregate(total=Sum("amount"))['total'] or 0
             
             request_qs = request_qs.values_list("request_master__id","request_master__created_at","item__name","amount","request_master__currency","exchange_rate","type") #,"request_master__commitment__company__name_ar"
 
-            payment_qs = TblCompanyPaymentDetail.objects.filter(
-                    payment_master__created_at__date__gte=from_dt,
-                    payment_master__created_at__date__lte=to_dt,
+            payment_qs_all = TblCompanyPaymentDetail.objects.filter(
                     payment_master__state=STATE_TYPE_CONFIRM,
                 ).annotate(
                     type=Value('payment', output_field=CharField()),
                 )
             
             if company:
-                payment_qs = payment_qs.filter(
+                payment_qs_all = payment_qs_all.filter(
                         payment_master__request__commitment__company=company,
                 )
             if currency:
-                payment_qs = payment_qs.filter(
+                payment_qs_all = payment_qs_all.filter(
                         payment_master__request__currency=currency,
                 )
+
+            payment_qs = payment_qs_all.filter(
+                    payment_master__created_at__date__range=(from_dt,to_dt)
+            )
             
             payment_total = payment_qs.aggregate(amount__sum=Sum(F("amount")*F("payment_master__exchange_rate")))['amount__sum']
+            payment_total_before = payment_qs_all \
+                .filter(payment_master__created_at__date__lt=from_dt) \
+                .aggregate(amount__sum=Sum(F("amount")*F("payment_master__exchange_rate")))['amount__sum'] or 0
 
             payment_qs = payment_qs.values_list("payment_master__id","payment_master__created_at","item__name","amount","payment_master__currency","payment_master__exchange_rate","type") #,"payment_master__request__commitment__company__name_ar"
 
             qs = request_qs.union(payment_qs).order_by("request_master__created_at")
 
             data = []
-            balance = 0
+            balance_opening = request_total_befor - payment_total_before
+            balance = balance_opening
             for d in qs:
                 a = list(d)
                 if a[6] == 'payment':
                     balance -= a[3]
                 else:
                     balance += a[3]
-
                 a.append(balance)
                 data.append(a)
 
             self.extra_context["form"] = form
-            self.extra_context["qs"] = data
+            self.extra_context["data"] = data
+            self.extra_context["request_opening"] = request_total_befor or 0
+            self.extra_context["payment_opening"] = payment_total_before or 0
+            self.extra_context["balance_opening"] = balance_opening or 0
             self.extra_context["request_total"] = request_total or 0
             self.extra_context["payment_total"] = payment_total or 0
             self.extra_context["balance_total"] = balance or 0
