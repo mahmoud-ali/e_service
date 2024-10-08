48fb20932b446f328c6c12759d6ec6b4735d5415
Mahmoud <mahmoud.webapps@gmail.com>
2024-10-08 14:47:05 +0200

    increase sheet_no length

--- company_profile/models.py
+++ company_profile/models.py
@@ -209,7 +209,7 @@ class TblCompanyProductionLicense(LoggingModel):
     state = models.ForeignKey(LkpState, on_delete=models.PROTECT,verbose_name=_("state"))
     locality = models.ForeignKey(LkpLocality, on_delete=models.PROTECT,verbose_name=_("locality"))
     location = models.CharField(_("location"),max_length=100)
-    sheet_no = models.CharField(_("sheet_no"),max_length=10,blank=True,null=True)
+    sheet_no = models.CharField(_("sheet_no"),max_length=20,blank=True,null=True)
     cordinates = models.TextField(_("cordinates"),max_length=256)
     mineral = models.ManyToManyField(LkpMineral,verbose_name=_("mineral"),default=[1])
     area = models.FloatField(_("Area in Kilometers"),blank=True,null=True)
