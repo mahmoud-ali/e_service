d2deb46c5bd569a60716ce645ae598e21ea36919
Mahmoud <mahmoud.webapps@gmail.com>
2024-11-05 11:26:18 +0200

    fix

--- gold_travel/templatetags/alloy_list.py
+++ gold_travel/templatetags/alloy_list.py
@@ -8,11 +8,11 @@ def total_alloy_weight(qs):
     total = 0
     for obj in qs:
         total += obj.appmovegolddetails_set.aggregate(sum=models.Sum("alloy_weight_in_gram"))['sum'] or 0
-    return total
+    return round(total,2)
 
 @register.filter(name="total_alloy_count")
 def total_alloy_count(qs):
     total = 0
     for obj in qs:
         total += obj.appmovegolddetails_set.count()
-    return total
+    return round(total,2)
