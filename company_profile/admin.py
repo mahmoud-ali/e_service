7f4a7f2a427d50a62ee29156d0673f718cd658b5
Mahmoud <mahmoud.webapps@gmail.com>
2024-12-03 10:30:50 +0200

    add email to search

--- company_profile/admin.py
+++ company_profile/admin.py
@@ -166,7 +166,7 @@ class TblCompanyProductionAdmin(ExportActionMixin,LoggingAdminMixin,admin.ModelA
      
     list_display = ["company_type","code","name_ar", "name_en", "status",'license_count','show_summary_link']
     list_filter = ["company_type","nationality","status","created_at",LicenseCountFilter,('email',admin.EmptyFieldListFilter)]
-    search_fields = ["code","name_ar","name_en"]
+    search_fields = ["code","name_ar","name_en","email"]
     exclude = ["created_at","created_by","updated_at","updated_by"]
     view_on_site = False
 
