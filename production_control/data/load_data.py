c5bae5a37cb38d414a6f0bd14fa19939c7702d9b
Mahmoud <mahmoud.webapps@gmail.com>
2024-12-18 14:05:22 +0200

    add moragib group

--- production_control/data/load_data.py
+++ production_control/data/load_data.py
@@ -1,6 +1,7 @@
 import csv
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 from company_profile.models import TblCompany
 
@@ -21,22 +22,29 @@ def import_moragbin(company_type=None,file_name='mokhlafat.csv'):
         next(reader, None)  # skip the headers
 
         User = get_user_model()
+        moragib_group = Group.objects.get(name='production_control_auditor')
+
         for row in reader:
             try:
                 code = row[0].strip()
                 name = row[1].strip()
                 email = row[2].strip()
 
-                user = User.objects.create_user(email, email, settings.ACCOUNT_DEFAULT_PASSWORD)
+                try:
+                    user = User.objects.get(email=email)
+                except:
+                    user = User.objects.create_user(email, email, settings.ACCOUNT_DEFAULT_PASSWORD)
+
+                moragib_group.user_set.add(user)
                 
-                moragib = LkpMoragib.objects.create(
+                moragib,created = LkpMoragib.objects.get_or_create(
                     user=user,
                     name=name,
                     emp_code=code,
                     company_type=company_type,
                 )
 
-                print(f"Created successfully, user: {user}, moragib: {moragib}")
+                print(f"Created successfully, user: {user}, moragib: {moragib}, created: {created}")
 
             except Exception as e:
                 print(f"id: {code}, Exception: {e}")
