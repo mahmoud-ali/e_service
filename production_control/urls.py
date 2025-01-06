74b51a581bc037491e0c9b4b0f2d0d3c92b6e48e
Mahmoud <mahmoud.webapps@gmail.com>
2025-01-06 11:11:20 +0200

    add production api

--- production_control/urls.py
+++ production_control/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from django.conf import settings
 
-from .views import ShippingView
+from .views import ProductionView, ShippingView
 
 
 app_name = "production"
 urlpatterns = [                                                        
+    path('production', ProductionView, name='production_production'),
     path('shipping', ShippingView, name='production_shipping'),
 
 ]
--- production_control/views.py
+++ production_control/views.py
@@ -2,9 +2,37 @@ from django.shortcuts import render
 from django.core import serializers
 from django.db.models import Prefetch
 from django.http import JsonResponse,HttpResponse
-from .models import STATE_CONFIRMED, GoldShippingForm
+from .models import STATE_CONFIRMED, GoldProductionForm, GoldShippingForm
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+@api_view(['GET'])
+def ProductionView(request):
+    qs = GoldProductionForm.objects.filter(state=STATE_CONFIRMED).prefetch_related(Prefetch('goldproductionformalloy_set')) #.only('company_id','goldshippingformalloy__id') #updated_at
+    qs_json = {'contents': 
+        map(
+            lambda obj: {
+                'id':obj.id, 
+                'company_id':obj.company_id,
+                'date':obj.date,
+                'form_no':obj.form_no,
+                'alloy_khabath':obj.alloy_khabath,
+                'alloy_remaind':obj.alloy_remaind,
+                'details':
+                        map(
+                            lambda detail: {
+                                'id':detail.id,                        
+                                'alloy_serial_no':detail.alloy_serial_no,                        
+                                'alloy_weight':detail.alloy_weight,                        
+                                'alloy_added_gold':detail.alloy_added_gold,                        
+                            },obj.goldproductionformalloy_set.all()
+                        )
+                },
+                qs
+            )
+    }
+
+    return Response( qs_json)
 
 @api_view(['GET'])
 def ShippingView(request):
@@ -16,19 +44,18 @@ def ShippingView(request):
                 'company_id':obj.company_id,
                 'date':obj.date,
                 'form_no':obj.form_no,
-                'state':obj.state,
                 'details':
                         map(
                             lambda detail: {
+                                'production_id':detail.alloy_serial_no.id,                        
                                 'alloy_serial_no':detail.alloy_serial_no.alloy_serial_no,                        
                                 'alloy_weight':detail.alloy_serial_no.alloy_weight,                        
                                 'alloy_added_gold':detail.alloy_serial_no.alloy_added_gold,                        
-                                'alloy_shipped':detail.alloy_serial_no.alloy_shipped,                        
                             },obj.goldshippingformalloy_set.all()
                         )
                 },
                 qs
             )
-    } #serializers.serialize('json', qs)
+    }
 
     return Response( qs_json)
