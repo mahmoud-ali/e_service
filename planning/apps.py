90080a8410b067dcf5b7e95a6790367d6bf5a7d5
Mahmoud <mahmoud.webapps@gmail.com>
2025-01-07 14:50:47 +0200

    translation

--- planning/apps.py
+++ planning/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class PlanningConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'planning'
+    verbose_name = _("Planning app")
