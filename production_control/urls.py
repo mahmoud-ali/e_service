19d707645f6f751fae6a96570e2d92067959b349
Mahmoud <mahmoud.webapps@gmail.com>
2025-01-13 15:11:35 +0200

    Update authentication endpoint to use token authentication and restore IsAuthenticated permission for ProductionView

--- production_control/urls.py
+++ production_control/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from django.conf import settings
-
+from rest_framework.authtoken import views
 from .views import ProductionView, ShippingView, Auth
 
 
 app_name = "production"
 urlpatterns = [                                                        
-    path('auth', Auth, name='production_production'),
+    path('auth', views.obtain_auth_token, name='production_production'),
     path('production', ProductionView, name='production_production'),
     path('shipping', ShippingView, name='production_shipping'),
 
