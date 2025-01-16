391275b9a1b4015e363dbd309a3ad48cab636ca6
Mahmoud <mahmoud.webapps@gmail.com>
2025-01-16 14:47:51 +0200

    Add MonthlyReport integration in task generation process

--- planning/cron.py
+++ planning/cron.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 
-from .models import TaskDuration, TaskExecution
+from .models import MonthelyReport, TaskDuration, TaskExecution
 
 admin_user = get_user_model().objects.get(id=1)
 
@@ -11,10 +11,16 @@ def generate_tasks():
     qs = TaskDuration.objects.filter(month=current_month,task__year=current_year)
     
     for obj in qs:
-        TaskExecution.objects.get_or_create(
-            task=obj.task,
+        month_rep = MonthelyReport.get_or_create(
             year=obj.task.year,
             month=obj.month,
+            created_by=admin_user,
+            updated_by=admin_user,
+        )
+
+        TaskExecution.objects.get_or_create(
+            report=month_rep,
+            task=obj.task,
             percentage=0,
             created_by=admin_user,
             updated_by=admin_user,
