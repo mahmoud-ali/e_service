45ac520d2ec34fee53e976d4527bcc7dc71e802c
Mahmoud <mahmoud.webapps@gmail.com>
2025-02-05 13:35:46 +0200

    fix: Remove Auth import from production_control URLs

--- production_control/urls.py
+++ production_control/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from rest_framework.authtoken import views
-from .views import ProductionView, ShippingView, Auth
+from .views import ProductionView, ShippingView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
