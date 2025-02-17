cfd4e825f342064fe8455c87461196b4abbc55a6
Mahmoud <mahmoud.webapps@gmail.com>
2025-02-17 15:48:24 +0200

    fix: Optimize task generation by consolidating MonthlyReport creation and removing existing TaskExecution records

--- planning/cron.py
+++ planning/cron.py
@@ -8,18 +8,18 @@ admin_user = get_user_model().objects.get(id=1)
 def generate_tasks():
     current_month,current_year = timezone.now().month,timezone.now().year
 
+    month_rep,_ = MonthelyReport.objects.get_or_create(
+        year=current_year,
+        month=current_month,
+        created_by=admin_user,
+        updated_by=admin_user,
+    )
+
+    TaskExecution.objects.filter(report=month_rep).delete()
+
     qs = TaskDuration.objects.filter(month=current_month,task__year=current_year)
     
     for obj in qs:
-        month_rep,_ = MonthelyReport.objects.get_or_create(
-            year=obj.task.year,
-            month=obj.month,
-            created_by=admin_user,
-            updated_by=admin_user,
-        )
-
-        TaskExecution.objects.filter(report=month_rep).delete()
-
         TaskExecution.objects.create(
             report=month_rep,
             task=obj.task,
