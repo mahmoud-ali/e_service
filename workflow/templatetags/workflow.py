09aaf706d1c6446740ee50f36af3b734d5436e51
Mahmoud <mahmoud.webapps@gmail.com>
2025-03-03 09:07:43 +0200

    fix: Add validation to get_next_states filter to handle None objects

--- workflow/templatetags/workflow.py
+++ workflow/templatetags/workflow.py
@@ -5,4 +5,7 @@ register = template.Library()
 
 @register.filter(name="get_next_states")
 def get_next_states(obj,user):
+    if obj is None or not hasattr(obj,'get_next_states'):
+        return []
+    
     return getattr(obj,'get_next_states')(user)
