c2ba35197ac4e728a81880579ceb6905fd7ca226
Mahmoud <mahmoud.webapps@gmail.com>
2025-03-09 16:39:10 +0200

    feat: Add Arabic translation for HSE Traditional app verbose name

--- hse_traditional/apps.py
+++ hse_traditional/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class HseTraditionalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hse_traditional'
+    verbose_name = _("hse traditional")
