ba5b3eff67806018074ef5bdfeecd6e4d6681101
Mahmoud <mahmoud.webapps@gmail.com>
2025-04-15 15:16:37 +0200

    fix: Update SRID for geom field in SmallMining model to 4326

--- check_cordinates/models.py
+++ check_cordinates/models.py
@@ -10,7 +10,7 @@ class SmallMining(models.Model):
     end_date = models.DateField(blank=True,null=True)
     area = models.FloatField(blank=True,null=True)
     nots = models.CharField(max_length=150,blank=True,null=True)
-    geom = models.MultiPolygonField(srid=32636)
+    geom = models.MultiPolygonField(srid=4326)
 
     def __str__(self):
         return f'{self.company_name} ({self.license_nu}) {self.geom.ewkt}'
