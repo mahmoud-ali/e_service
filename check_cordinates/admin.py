ca924e526445e0e7fcdf1b54a2b724609cc44211
Mahmoud <mahmoud.webapps@gmail.com>
2025-05-14 07:48:09 +0200

    feat: Switch SmallMiningAdmin to use LeafletGeoAdmin for enhanced mapping capabilities

--- check_cordinates/admin.py
+++ check_cordinates/admin.py
@@ -1,7 +1,9 @@
 from django.contrib.gis import admin
+from leaflet.admin import LeafletGeoAdmin
+
 from .models import SmallMining
 
-class SmallMiningAdmin(admin.GISModelAdmin):
+class SmallMiningAdmin(LeafletGeoAdmin):
     model = SmallMining
     list_display = ["license_nu","company_name","state","mineral"]
     search_fields = ["license_nu","company_name","state","mineral"]
