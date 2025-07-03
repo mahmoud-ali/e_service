a9a50fa2b5661fcc8b010003bf84dc6f842d20db
Mahmoud <mahmoud.webapps@gmail.com>
2025-07-03 17:05:13 +0200

    feat: Add verbose_name to app configurations for better clarity in admin interface

--- company_profile_exploration/apps.py
+++ company_profile_exploration/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CompanyProfileExplorationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'company_profile_exploration'
+    verbose_name = "ا.ع للإشراف والرقابة على شركات الاستكشاف"
--- it/apps.py
+++ it/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ItConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'it'
+    verbose_name = "إدارة تقنية المعلومات"
