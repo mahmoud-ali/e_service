799458aa5dff801bb0772cd118468596a9f7b40d
mahmoud <mahmoud.ali@smrc.sd>
2025-07-16 17:57:54 +0200

    link device with user

--- fleet/models/__init__.py
+++ fleet/models/__init__.py
@@ -3,6 +3,9 @@ from django.utils.translation import gettext_lazy as _
 from .fleet import *
 from .traccar import *
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class VehicleGPSDevice(LoggingModel):
     vehicle = models.OneToOneField(Vehicle, on_delete=models.CASCADE,verbose_name=_("المركبة"))
     gps = models.OneToOneField(TcDevices, on_delete=models.CASCADE,verbose_name=_("جهاز التتبع"))
@@ -17,7 +20,7 @@ class VehicleGPSDevice(LoggingModel):
                 self.gps.phone = driver.phone
             except:
                 pass
-            
+
             self.gps.model = self.vehicle.model.name
             self.gps.save()
         except Exception as e:
@@ -29,3 +32,12 @@ class VehicleGPSDevice(LoggingModel):
     class Meta:
         verbose_name = _("جهاز تتبع مركبة")
         verbose_name_plural = _("اجهزة تتبع المركبات")
+
+@receiver(post_save, sender=TcDevices)
+def link_gps_device_with_users(sender, instance, **kwargs):
+    groups = Group.objects.filter(name__in=['fleet_employee', ])
+    users_emails = User.objects.filter(groups__in=groups).distinct().values_list('email', flat=True)
+
+    with connection.cursor() as cursor:
+        for tc_user in traccar.TcUsers.objects.filter(email__in=users_emails):
+            cursor.execute(f"INSERT INTO tc_user_driver (userid,driverid) VALUES ({tc_user.id},{instance.id})") #,[tc_user.id,tc_driver.id]")
