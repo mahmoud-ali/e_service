a0d0306cb86d6b82658668b9e81287f8d15a0998
Mahmoud <mahmoud.webapps@gmail.com>
2025-07-24 08:11:57 +0200

    add soug geojson layer for test

--- traditional_app/urls.py
+++ traditional_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from traditional_app.views import PayrollT3agood
+from traditional_app.views import PayrollT3agood, geojson_soug_view
 
 app_name = "traditional_app"
 urlpatterns = [                                                        
     path('payroll_t3agood/', PayrollT3agood.as_view(), name='payroll_t3agood'),
+    path('soug_layer/', geojson_soug_view, name='soug_layer'),
 ]
