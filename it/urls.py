8e0c8430b079749c28b998bba1684bb9e566c93c
mahmoud <mahmoud.ali@smrc.sd>
2025-08-19 11:01:14 +0200

    fix

--- it/urls.py
+++ it/urls.py
@@ -5,5 +5,5 @@ from it.views import EmployeeComputerAskAIView, HelpdeskTelegramUser
 app_name = "it"
 urlpatterns = [      
     path('ai/<int:pk>/', EmployeeComputerAskAIView.as_view(), name='ai_prompt'),
-    path('help_desk_form/<int:user_id>/', HelpdeskTelegramUser.as_view(), name='help_desk_form'),
+    path('help_desk_form/<uuid:user_id>/', HelpdeskTelegramUser.as_view(), name='help_desk_form'),
 ]
