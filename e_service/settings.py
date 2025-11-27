f55f4696d5264b4bf73bd0a5db34c9d8401a1970
Mahmoud <mahmoud.webapps@gmail.com>
2025-11-27 10:08:41 +0200

    add auditlog app

--- e_service/settings.py
+++ e_service/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = [
     # 'pwa',
 
     'leaflet',
+    'auditlog',
     
     # Local                
     'accounts',
@@ -138,6 +139,7 @@ MIDDLEWARE = [
     #3rd party
     'allauth.account.middleware.AccountMiddleware',
     'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
+    'auditlog.middleware.AuditlogMiddleware',
 
     # Debug
     'debug_toolbar.middleware.DebugToolbarMiddleware',
@@ -318,6 +320,11 @@ ACCOUNT_DEFAULT_PASSWORD = "changethispassword"
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 8000
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10 MB
+
+# Auditlog
+AUDITLOG_INCLUDE_ALL_MODELS=True
+AUDITLOG_DISABLE_REMOTE_ADDR = True
+
 # logging 
 LOGGING = {
     'version': 1,
