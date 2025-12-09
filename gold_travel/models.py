c8dd4797235a94fc2ae9133037665542aea59a9b
Mahmoud <mahmoud.webapps@gmail.com>
2025-12-09 08:33:21 +0200

    remove log access by mixin

--- gold_travel/models.py
+++ gold_travel/models.py
@@ -2,11 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
-from auditlog.mixins import LogAccessMixin
-
 from company_profile.models import LkpState
 
-class LoggingModel(LogAccessMixin,models.Model):
+class LoggingModel(models.Model):
     """
     An abstract base class model that provides self-
     updating ``created_at`` and ``updated_at`` fields for responsable user.
--- workflow/model_utils.py
+++ workflow/model_utils.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
-from auditlog.mixins import LogAccessMixin
-
-class LoggingModel(LogAccessMixin,models.Model):
+class LoggingModel(models.Model):
     """
         updating ``created_at`` and ``updated_at`` fields for responsable user.
     """
