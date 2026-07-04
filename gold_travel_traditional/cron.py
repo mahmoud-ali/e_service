10c5d42e2ceca4a8c92d684e25e478dafdfd807f
Mahmoud <mahmoud.webapps@gmail.com>
2026-07-04 11:01:06 +0200

    feat: refine expired_app logic to separate state filters for clarity

--- gold_travel_traditional/cron.py
+++ gold_travel_traditional/cron.py
@@ -5,6 +5,10 @@ def expired_app():
     before_three_days = (timezone.now() - timezone.timedelta(days=3)).date()
 
     AppMoveGoldTraditional.objects\
-        .filter(state__in=[AppMoveGoldTraditional.STATE_NEW,AppMoveGoldTraditional.STATE_RENEW],issue_date__lt=before_three_days)\
+        .filter(state__in=[AppMoveGoldTraditional.STATE_NEW,],issue_date__lt=before_three_days)\
+        .update(state=AppMoveGoldTraditional.STATE_EXPIRED)
+    
+    AppMoveGoldTraditional.objects\
+        .filter(state__in=[AppMoveGoldTraditional.STATE_RENEW,],renew_date__lt=before_three_days)\
         .update(state=AppMoveGoldTraditional.STATE_EXPIRED)
     
