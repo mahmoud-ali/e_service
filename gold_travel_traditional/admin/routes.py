2d4a2d906789137bfbe8fd7b71623d1b490d20b1
Mahmoud <mahmoud.webapps@gmail.com>
2026-08-06 13:19:01 +0200

    fix: update list_filter in RouteAdmin to include wijhat_altarhil

--- gold_travel_traditional/admin/routes.py
+++ gold_travel_traditional/admin/routes.py
@@ -5,7 +5,7 @@ from gold_travel_traditional.models import Route
 
 class RouteAdmin(admin.ModelAdmin):
     list_display = ['jihat_alaisdar', 'wijhat_altarhil']
-    list_filter = ['jihat_alaisdar__state', 'jihat_alaisdar']
+    list_filter = ['jihat_alaisdar__state', 'wijhat_altarhil', 'jihat_alaisdar']
     search_fields = ['jihat_alaisdar__name', 'wijhat_altarhil__name']
     autocomplete_fields = ['jihat_alaisdar', 'wijhat_altarhil']
 
